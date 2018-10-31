be3d232b04ea1d9f10278e03748b244c94b2e6ad
zhangruibin <zhangruibin@yyxtech.com>
2018-10-31 17:53:59 +0800

    @Value

--- src/main/java/com/example/entity/Book.java
+++ src/main/java/com/example/entity/Book.java
@@ -2,18 +2,22 @@ package com.example.entity;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by zhangrui on 2017/11/6.
  */
 @Component
+@Configuration
 @ApiModel(value = "Book",description = "图书")
 public class Book {
     @ApiModelProperty(value = "图书编号")
     private Long id;
     @ApiModelProperty(value = "图书名称")
+    @Value("${aaaa}")
     private String name ;
     @ApiModelProperty(value = "图书价格")
     private double price;
