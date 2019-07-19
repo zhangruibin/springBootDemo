8e5e21d04a9b5ef5da25f5ff832ad9739ef9ed90
zhangruibin <itbin@foxmail.com>
2019-07-19 10:59:25 +0800

    NIO BLANK;

---
+++ src/main/java/com/example/test/IO/aboutIO/TestSocketChannel.java
@@ -0,0 +1,22 @@
+package com.example.test.IO.aboutIO;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @ClassName TestSocketChannel
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2019/7/17 15:07
+ * @Version
+ */
+public class TestSocketChannel {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
